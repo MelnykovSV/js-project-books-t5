660853024db81c1d5eaf6de034c799c43a93bd82
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-05-12 18:58:23 +0300

    add overlay on book`s image

--- src/js/components/best-sellers/markupTopBooks.js
+++ src/js/components/best-sellers/markupTopBooks.js
@@ -19,11 +19,16 @@ export function createMarkupAllBooks(data) {
 function createMarkupBook(books) {
   return books
     .map(
-      ({ book_image, title, author, _id }) => `
+      ({ book_image, title, author, _id, description }) => `
   <li class="books__item list" data-book-id="${_id}">
+    <div class="books__overlay">
     <img class="books__image" src="${book_image}" alt="book ${title}" />
-    <h4 class="books__title">${title}</h4>
-    <p class="books__text">${author}</p>
+    <p class="books__text--overlay">${
+      description || 'Sorry, book`s description not found'
+    }</p>
+    </div>
+    <h4 class="books__title">${title || 'Not found'}</h4>
+    <p class="books__text">${author || 'Not found'}</p>
   </li>
 `
     )
