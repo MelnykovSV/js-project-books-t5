6f2c19dcccd233c0c4db393e5396fba17e481574
MelnykovSV <svmelnykov@gmail.com>
2023-05-13 18:43:14 +0300

    user updates merged

---
+++ src/js/firebase/auth.js
@@ -0,0 +1,8 @@
+import globalState from '../globalState';
+import authUtils from '../firebase/firebaseAuth';
+
+const signUpForm = document.querySelector('.sign-up-form');
+const signInForm = document.querySelector('.sign-in-form');
+
+const testButton = document.querySelector('.test-button');
+const stateButton = document.querySelector('.state-button');
