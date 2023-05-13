3c7a3019a2f0b3e4743799205bbef7353333a507
ForestDr <nikita2001395@gmail.com>
2023-05-13 21:31:37 +0300

    remove bg close

--- src/js/components/form/formModal.js
+++ src/js/components/form/formModal.js
@@ -11,19 +11,14 @@ function showModal() {
   document.body.classList.add('body-lock');
   modalEl.classList.remove('backdrop-form--is-hidden');
 
-  modalEl.addEventListener('click', hideModal);
   window.addEventListener('keydown', hideModal);
 }
 
 function hideModal(e) {
-  console.log(e.target);
-
-  console.log(e.target.classList.contains('backdrop-form'));
-  if (e.target.classList.contains('backdrop-form') || e.code === 'Escape') {
+  if (e.code === 'Escape') {
     document.body.classList.remove('body-lock');
     modalEl.classList.add('backdrop-form--is-hidden');
 
-    modalEl.removeEventListener('click', hideModal);
     window.removeEventListener('keydown', hideModal);
   }
 }
