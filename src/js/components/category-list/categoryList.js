98293b14f9f8492d0c2915aff9295867548defde
Anastasia27041 <anastasiaglyvka@gmail.com>
2023-05-14 14:38:26 +0300

    add css for scrollbar

--- src/js/components/category-list/categoryList.js
+++ src/js/components/category-list/categoryList.js
@@ -1,12 +1,16 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 import { createMarkupOfCategoryList } from './markupCategotyList';
 import { fetchCategoryList } from '../../api';
+
+import ResizeObserver from 'resize-observer-polyfill';
+window.ResizeObserver = ResizeObserver;
 
 const categoryList = document.querySelector('.categories-links-list');
 
 async function getCategoryList() {
   const { data } = await fetchCategoryList();
-  console.log(data);
   categoryList.insertAdjacentHTML(
     'beforeend',
     createMarkupOfCategoryList(data)
