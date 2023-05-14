f62d39cf62d63e0b5b6df694f3055f1c117697ce
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-05-14 13:39:27 +0000

    Deploying to gh-pages from @ MelnykovSV/js-project-books-t5@8bf5eb3cf79ffc135ba5ccfe3538db340f204083 🚀

---
+++ shopping-list.9f8c29d2.js
@@ -0,0 +1,2 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},r={},n={},o=e.parcelRequire8630;null==o&&((o=function(e){if(e in r)return r[e].exports;if(e in n){var o=n[e];delete n[e];var i={id:e,exports:{}};return r[e]=i,o.call(i.exports,i,i.exports),i.exports}var l=new Error("Cannot find module '"+e+"'");throw l.code="MODULE_NOT_FOUND",l}).register=function(e,r){n[e]=r},e.parcelRequire8630=o),o("alV1T"),o("kZ6wr");
+//# sourceMappingURL=shopping-list.9f8c29d2.js.map
