a991aed413f1d7d2d40fbfc7612ddacc0f14c5ec
MelnykovSV <svmelnykov@gmail.com>
2023-05-16 01:16:33 +0300

    header auth done

--- src/shoppingList.js
+++ src/shoppingList.js
@@ -1,5 +1,5 @@
 import './js/components/header/index';
-import './js/firebase/auth';
+import './js/firebase/shoppingListAuth';
 import './js/components/support/index';
 
 import './js/components/shopping-list/cart';
