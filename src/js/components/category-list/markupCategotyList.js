e8e09a2b6835636caa6322292da0347aa10df585
ForestDr <nikita2001395@gmail.com>
2023-05-17 19:18:38 +0300

    fix

--- src/js/components/category-list/markupCategotyList.js
+++ src/js/components/category-list/markupCategotyList.js
@@ -2,10 +2,8 @@
 export function createMarkupOfCategoryList(books) {
   return books
     .map(({ list_name }) => {
-      const attributeValue = list_name.split(' ').join('');
-
       return `<li class="categories-links-list__item">
-              <a href="#" class="category-link" data-category=${attributeValue}>${list_name}</a>
+              <a href="#" class="category-link" data-category="${list_name}">${list_name}</a>
             </li>`;
     })
     .join('');
