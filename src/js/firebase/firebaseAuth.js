a3f13250b3173390a8ec91424d5eddb27f73a98e
MelnykovSV <svmelnykov@gmail.com>
2023-05-18 04:20:37 +0300

    1

--- src/js/firebase/firebaseAuth.js
+++ src/js/firebase/firebaseAuth.js
@@ -202,13 +202,12 @@ class FirebaseAuth {
         // User is signed out
         userStatus = false;
         localStorage.removeItem('userData');
-        // if (localStorage.getItem('globalState')) {
-        //   globalState.set(JSON.parse(localStorage.getItem('globalState')));
-        // }
-        // else {
-        //   globalState.set(INITIAL_STATE_VALUE);
-        //   globalState.writeToLocalStorage();
-        // }
+        if (localStorage.getItem('globalState')) {
+          globalState.set(JSON.parse(localStorage.getItem('globalState')));
+        } else {
+          globalState.set(INITIAL_STATE_VALUE);
+          globalState.writeToLocalStorage();
+        }
 
         authComponent.classList.add('signed-out');
         closingCartCounter();
